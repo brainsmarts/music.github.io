fe8bfee38f30786b9f57a9d02e889cdd334b4d17
brainsmarts <brianaprilgaming@gmail.com>
2023-07-23 13:24:17 -0400

    Testing Commit
    The use of this Commit is to simply test out commiting
    
    the only thing changed is //abcdefg

--- scalegame.js
+++ scalegame.js
@@ -3,6 +3,7 @@
 //moving on it would be easier to have a method that loops any amount of time, the starting note, and the scale pattern
 //const obj = {"A":0,"A#":1,"B":2,"C":3,"C#":4,"D":5,"D#":6,"E":7,"F":8,"F#":9,"G":10,"G#":11};
 //would be good to move to json file but for now have it as a constant here
+//abcdefg
 const noteList = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
 
 const obj2 = {0:"A",1:"A#",2:"B",3:"C",4:"C#",5:"D",6:"D#",7:"E",8:"F",9:"F#",10:"G",11:"G#"};//this is stupid
