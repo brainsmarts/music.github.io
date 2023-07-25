74c1fac67edd122ab0117ce123e58d5f37b6cb54
brainbun <aprilbriancai@gmail.com>
2023-07-24 22:46:08 -0400

    Update 3
    Updates the scalesgame.js and buttons.css

--- javascript/scalesgame.js
+++ javascript/scalesgame.js
@@ -3,23 +3,26 @@
 //12 buttons to press
 const gameDisplay = document.getElementById("GameInputDisplayButtons");
 const gameInput = document.getElementById("GameInputButtons");
+var selectedButton;
 
 function initialize(){
     console.log("Initialization Process Has Been Called");
     for(var i = 0; i < 7; i++){
-        var indexAttribute = document.createAttribute("index");
+        //var indexAttribute = document.createAttribute("index");
         var selectedAttribute = document.createAttribute("selected");
         var newButton = document.createElement("button");
         gameDisplay.appendChild(newButton);
-        newButton.createAttribute
-        indexAttribute.value = i;
-        newButton.setAttributeNode(indexAttribute);
+        //newButton.createAttribute
+        //indexAttribute.value = i;
+        newButton.id = "display"+i;
+        //newButton.setAttributeNode(indexAttribute);
         newButton.innerHTML = "";
         newButton.className = "display";
         selectedAttribute.value = "false";
-        newButton.setAttributeNode(selectedAttribute);
-        
+        newButton.setAttributeNode(selectedAttribute);   
     }
+
+    document.getElementById("display0").setAttribute("selected","true"); 
 
     for(var i = 0; i < 12; i++){
         var newButton = document.createElement("button");
@@ -30,6 +33,8 @@ function initialize(){
     }
 }
 
+
+
 function randomizeScale(){
     var random = Math.floor(Math.random()*12); //returns 0 - 11
     var text = document.getElementById("testingText");
@@ -37,6 +42,9 @@ function randomizeScale(){
     
 }
 
+
+//when an input is clicked, it will put the appropriate value and then select the 
+//next element to be the selected one
 function onNoteInput(){
     //console.log(this.innerHTML);
 }
