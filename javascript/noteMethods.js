bf8d4af06ef82f6e405722b72eec4ec770592dab
brainsmarts <brianaprilgaming@gmail.com>
2024-01-08 21:55:27 -0500

    I really dont remember but i added alot of things

--- javascript/noteMethods.js
+++ javascript/noteMethods.js
@@ -1,34 +1,98 @@
-//to find scale based on the scale pattern 
+//to find scale based on the scale pattern
 //ex majorScale >> W W H W W W where each W = 2 and H = 1
 //moving on it would be easier to have a method that loops any amount of time, the starting note, and the scale pattern
 //const obj = {"A":0,"A#":1,"B":2,"C":3,"C#":4,"D":5,"D#":6,"E":7,"F":8,"F#":9,"G":10,"G#":11};
 //would be good to move to json file but for now have it as a constant here
 //abcdefghijklmnopasdfjashdf
-const noteList = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
+const minor = [2, 1, 2, 2, 1, 2, 2];
+const major = [2, 2, 1, 2, 2, 2, 1];
+const noteListWithAlternates = [
+  ["A"],
+  ["A#","B♭"],
+  ["B","C♭"],
+  ["C","B#"],
+  ["C#","D♭"],
+  ["D"],
+  ["D#","F♭"],
+  ["E","F♭"],
+  ["F","E#"],
+  ["F#","G♭"],
+  ["G"]
+  ["G#","A♭"]
+]
+const noteList = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+];
+const scalePatternRandomize = [
+  [minor, major],
+  ["Minor", "Major"],
+];
 
-const obj2 = {0:"A",1:"A#",2:"B",3:"C",4:"C#",5:"D",6:"D#",7:"E",8:"F",9:"F#",10:"G",11:"G#"};
-const noteListPositionFinder = {"A":0,"A#":1,"B":2,"C":3,"C#":4,"D":5,"D#":6,"E":7,"F":8,"F#":9,"G":10,"G#":11};//this is stupid
-const minor = [2,1,2,2,1,2,2];
-const major = [2,2,1,2,2,2,1];
-const scalePatternRandomize = [[minor,major],["Minor","Major"]];
 
 //import "../noteSets.json";
 
-function createNoteSet(scalenote, pattern){ //int and int array
-    var newNoteSet = [];    
-    var iterator = scalenote;
+function createNoteSet(scalenote, pattern) {
+  //int and int array
+  var newNoteSet = [];
+  var iterator = scalenote;
 
-    for(let i = 0; i < pattern.length; i++){
-        if(iterator > 11)
-            iterator -= 12;
-        newNoteSet.push(noteList[iterator]);
-        iterator += pattern[i];
- 
-    }
-    return newNoteSet;
+  for (let i = 0; i < pattern.length; i++) {
+    if (iterator > 11) iterator -= 12;
+    newNoteSet.push(noteList[iterator]);
+    iterator += pattern[i];
+  }
+  return newNoteSet;
 }
 
 
+//assume there are two octives of notes
+/*
+first make note of which note was played last, if it was the last three notes 
+and the next note being played is the first three notes, then play an octive higher,
+else switch back down
+if it is already an octive higher then reset back to the first octive 
+ */
+//D# is the nicest sounding note
+
+
+const audioContext = new AudioContext();
+const audioElement = document.querySelector("audio"); 
+const track = audioContext.createMediaElementSource(audioElement);
+
+
+function playNote(note) {
+  track.connect(audioContext.destination);
+  audioElement.play();
+  console.log("played");
+  /*
+  const buffer = new AudioBuffer({
+    numberOfChannels: 2,
+    length: audio.sampleRate * 2.0,
+    sampleRate: audioCtx.sampleRate,
+  });
+  const audio = context.createBufferSource();
+  audio.buffer = buffer;
+  audio.playbackRate.value = 2;
+  audio.connect(context.destination);
+  audio.start(0); */
+ 
+}
+function loadSample(){
+  return fetch("./audiofiles/D-.mp3")
+    .then(response => response.arrayBuffer())
+    .then(buffer => context.decodeAudioData(buffer));
+}
 //export {notelist,createNoteSet};
 
 /*
@@ -44,3 +108,25 @@ function test(){
     console.log("has been accessed");
 }*/
 
+const noteMP3 = {
+  "A": "A.mp3",
+  "A#": "A-.mp3",
+  "B♭": "A-.mp3",
+  "B": "B.mp3",
+  "C": "C.mp3",
+  "B#": "C.mp3",
+  "C#": "C-.mp3",
+  "D♭": "C-.mp3",
+  "D": "D.mp3",
+  "D#": "D-.mp3",
+  "E♭": "D-.mp3",
+  "E": "E.mp3",
+  "F♭": "E.mp3",
+  "F": "F.mp3",
+  "E#": "F.mp3",
+  "F#": "F-.mp3",
+  "G♭": "F-.mp3",
+  "G": "G.mp3",
+  "G#": "G-.mp3",
+  "A♭": "G-.mp3",
+};
--- javascript/scalesgame.js
+++ javascript/scalesgame.js
@@ -1,6 +1,5 @@
-
-//7 buttons in the middle with nothing inside
-//12 buttons to press
+// 7 buttons in the middle with nothing inside
+// 12 buttons to press
 const gameDisplay = document.getElementById("input_display_container");
 const gameInput = document.getElementById("input_button_container");
 const gameResult = document.getElementById("result_container");
@@ -9,122 +8,162 @@ var selectedButton;
 var randomizedScale;
 const numOfDisplay = 7;
 
-function initialize(){
-    console.log("Initialization Process Has Been Called");
-    for(var i = 0; i < numOfDisplay; i++){
-        var newDisplayButton = document.createElement("button");
+function initialize() {
+  console.log("Initialization Process Has Been Called");
+  createDisplayButtons();
+  createResultContent();
+  createInputButtons();
 
-        var indexAttribute = document.createAttribute("index");
-        indexAttribute.value = i;
+  randomizeScale();
+  document
+    .getElementById("finished_button")
+    .addEventListener("click", finished);
+  document.getElementById("restart_button").addEventListener("click", restart);
+}
 
-        var selectedAttribute = document.createAttribute("selected");
-        selectedAttribute.value = "false";
-        if (i == 0){
-            selectedAttribute.value = "true";
-            selectedButton = newDisplayButton;
-        }
+function createInputButtons() {
+  for (var i = 0; i < 12; i++) {
+    let newInputButton = document.createElement("button");
+    newInputButton.className = "input_button";
+    newInputButton.id = "input_button-" + i;
+    newInputButton.innerHTML = noteList[i];
+    newInputButton.addEventListener("click", onNoteInput);
+    gameInput.appendChild(newInputButton);
+  }
+}
 
-        newDisplayButton.innerHTML = "";
-        newDisplayButton.className = "display_button";
- 
-        newDisplayButton.setAttributeNode(indexAttribute);
-        newDisplayButton.setAttributeNode(selectedAttribute);   
+function createResultContent() {
+  for (var i = 0; i < numOfDisplay; i++) {
+    let displayContainer = document.createElement("div");
+    displayContainer.className = "result_item_container";
+    displayContainer.id = "result_item_container-" + i;
+    let resultPassAttribute = document.createAttribute("pass");
+    resultPassAttribute.value = "true";
+    displayContainer.setAttributeNode(resultPassAttribute);
 
-        newDisplayButton.id = "display_button-"+i;     
-        newDisplayButton.addEventListener("click",selectDisplayInput);
-        gameDisplay.appendChild(newDisplayButton);
-        
-        var displayContainer = document.createElement("div");
-        displayContainer.className = "result_item_container";
-        var newResultDisplay = document.createElement("p");
-        newResultDisplay.className = "result_item";
-        newResultDisplay.innerHTML = "hi"+i;    
-        displayContainer.appendChild(newResultDisplay);    
-        gameResult.appendChild(displayContainer);
+    let newResultDisplay = document.createElement("p");
+    newResultDisplay.className = "result_item";
+    newResultDisplay.id = "result_item-" + i;
 
+    newResultDisplay.innerHTML = "hi" + i;
+    displayContainer.appendChild(newResultDisplay);
+    gameResult.appendChild(displayContainer);
+  }
+}
+
+function createDisplayButtons() {
+  for (var i = 0; i < numOfDisplay; i++) {
+    let newDisplayButton = document.createElement("button");
+
+    let indexAttribute = document.createAttribute("index");
+    indexAttribute.value = i;
+
+    let selectedAttribute = document.createAttribute("selected");
+    selectedAttribute.value = "false";
+    if (i == 0) {
+      selectedAttribute.value = "true";
+      selectedButton = newDisplayButton;
     }
 
-    //selectedButton = document.ge
+    newDisplayButton.innerHTML = "";
+    newDisplayButton.className = "display_button";
 
-    for(var i = 0; i < 12; i++){
-        var newInputButton = document.createElement("button");
-        newInputButton.className = "input_button";
-        newInputButton.id = "input_button-"+i;
-        newInputButton.innerHTML = noteList[i];
-        newInputButton.addEventListener("click",onNoteInput);
-        gameInput.appendChild(newInputButton);
+    newDisplayButton.setAttributeNode(indexAttribute);
+    newDisplayButton.setAttributeNode(selectedAttribute);
+
+    newDisplayButton.id = "display_button-" + i;
+    newDisplayButton.addEventListener("click", selectDisplayInput);
+    gameDisplay.appendChild(newDisplayButton);
+  }
+}
+
+function randomizeScale() {
+  var randomNote = Math.floor(Math.random() * 12); // returns 0 - 11
+  var randomPattern = Math.floor(Math.random() * 2);
+  randomizedScale = createNoteSet(
+    randomNote,
+    scalePatternRandomize[0][randomPattern]
+  );
+  document.getElementById("scale_text_content").innerHTML =
+    scalePatternRandomize[1][randomPattern] +
+    " " +
+    randomizedScale[0] +
+    " Scale";
+}
+
+function selectDisplayInput() {
+  selectedButton.setAttribute("selected", "false");
+  selectedButton = this;
+  selectedButton.setAttribute("selected", "true");
+}
+
+function selectDisplayInputID(newSelecterButton) {
+  selectedButton.setAttribute("selected", "false");
+  selectedButton = selectedButton = document.getElementById(newSelecterButton);
+  selectedButton.setAttribute("selected", "true");
+}
+
+// when an input is clicked, it will put the appropriate value and then select the
+// next element to be the selected one
+function onNoteInput() {
+  if (gameOver) return;
+  playNote(this.innerHTML);
+  var index = selectedButton.getAttribute("index");
+
+  selectedButton.innerHTML = this.innerHTML;
+
+  if (parseInt(index) + 1 < numOfDisplay) {
+    selectedButton.setAttribute("selected", "false");
+    selectedButton = document.getElementById(
+      "display_button-" + (parseInt(index) + 1)
+    );
+    selectedButton.setAttribute("selected", "true");
+  }
+}
+
+function finished() {
+  gameOver = true;
+  var win = true;
+  var score = numOfDisplay;
+
+  for (var i = 0; i < numOfDisplay; i++) {
+    if (
+      document
+        .getElementById("display_button-" + i)
+        .innerHTML.localeCompare(randomizedScale[i]) == 0
+    ) {
+      document.getElementById("result_item-" + i).innerHTML =
+        randomizedScale[i];
+      document
+        .getElementById("result_item_container-" + i)
+        .setAttribute("pass", "true");
+    } else {
+      document.getElementById("result_item-" + i).innerHTML =
+        randomizedScale[i];
+      document
+        .getElementById("result_item_container-" + i)
+        .setAttribute("pass", "false");
+      win = false;
+      score -= 1;
     }
-
-    randomizeScale();
-    document.getElementById("finished_button").addEventListener("click", finished);
-    document.getElementById("restart_button").addEventListener("click", restart);
+  }
+  document.getElementById("result_container").setAttribute("hidden", "false");
+  document.getElementById("scale_text_content").setAttribute("hidden", "true");
+  document
+    .getElementById("input_button_container")
+    .setAttribute("hidden", "true");
 }
 
-function randomizeScale(){
-    var randomNote = Math.floor(Math.random()*12); //returns 0 - 11
-    var randomPattern = Math.floor(Math.random()*2);
-    randomizedScale = createNoteSet(randomNote,scalePatternRandomize[0][randomPattern]);
-    document.getElementById("scale_text_content").innerHTML = scalePatternRandomize[1][randomPattern] + " " + randomizedScale[0] + " Scale";
-}
-
-function selectDisplayInput(){
-    selectedButton.setAttribute("selected","false");
-    selectedButton = this;
-    selectedButton.setAttribute("selected","true");
-}
-
-function selectDisplayInputID(newSelecterButton){
-    selectedButton.setAttribute("selected","false");
-    selectedButton = selectedButton = document.getElementById(newSelecterButton);
-    selectedButton.setAttribute("selected","true");
-}
-
-//when an input is clicked, it will put the appropriate value and then select the 
-//next element to be the selected one
-function onNoteInput(){
-    if (gameOver)
-        return;
-
-    var index = selectedButton.getAttribute("index");
-    for(var i = 0; i < numOfDisplay; i++){
-        var string = i+"";
-        if (string == index){
-            selectedButton.innerHTML = this.innerHTML;
-            if (i+1 < numOfDisplay){
-            selectedButton.setAttribute("selected","false");
-            selectedButton = document.getElementById("display_button-"+(i+1));
-            selectedButton.setAttribute("selected","true");
-            }
-        }
-    }
-}
-
-function finished(){
-    gameOver = true;
-    var win = true;
-    var score = numOfDisplay;
-    var result = "";
-    for (var i = 0; i < numOfDisplay; i++){
-        if(document.getElementById("display_button-"+i).innerHTML.localeCompare(randomizedScale[i]) == 0){
-            
-            result += document.getElementById("display_button-"+i).innerHTML + " " + randomizedScale[i] + " pass\n";
-        }else{
-            result += document.getElementById("display_button-"+i).innerHTML + " " + randomizedScale[i] + " fail\n";
-            win = false;
-            score -= 1;
-        }
-    }
-    result += randomizedScale;
-    result += " " + score;
-    document.getElementById("result_container").setAttribute("hidden","false");
-}
-
-function restart(){
-    for(var i = 0; i < numOfDisplay; i++){
-        document.getElementById("display_button-"+i).innerHTML = "";
-    }
-    document.getElementById("result_container").setAttribute("hidden","true");
-    randomizeScale();
-    selectDisplayInputID("display_button-0");
-    gameOver = false;
+function restart() {
+  for (var i = 0; i < numOfDisplay; i++) {
+    document.getElementById("display_button-" + i).innerHTML = "";
+  }
+  randomizeScale();
+  selectDisplayInputID("display_button-0");
+  document.getElementById("result_container").setAttribute("hidden", "true");
+  document.getElementById("scale_text_content").setAttribute("hidden", "false");
+  document
+    .getElementById("input_button_container")
+    .setAttribute("hidden", "false");
+  gameOver = false;
 }
